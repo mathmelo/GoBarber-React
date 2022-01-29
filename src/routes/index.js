9e014f5fdb8c8d36854bb24f4f3c8f19f2c45c86
Matheus Melo <dev.matheus.melo@gmail.com>
2022-01-29 10:37:41 -0300

    Private Routes
    + Private routes middleware added

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,17 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
+import PrivateWrapper from './Route';
 
 export default function routes() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="register" element={<SignUp />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="profile" element={<Profile />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/" element={<PrivateWrapper />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
     </Routes>
   );
 }
