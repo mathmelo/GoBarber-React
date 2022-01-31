e0ac1717ba7ffe6d05fa1fcdedb2d399683b6210
Matheus Melo <dev.matheus.melo@gmail.com>
2022-01-31 15:35:57 -0300

    Authentication styling
    + polished
    + SignIn and SignUp styles added

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="button">Criar conta</button>
+        <Link to="/login">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
