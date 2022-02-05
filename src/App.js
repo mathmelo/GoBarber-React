766daa51ab75b3ab4f2ba264b22a129774c8ca36
Matheus Melo <dev.matheus.melo@gmail.com>
2022-02-05 16:15:12 -0300

    Persisting authentication
    + Persisting authentication using redux-persist (local storage)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes/index';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes />
-        <GlobalStyle />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-import store from '~/store';
+import { store } from '~/store';
 
 export default function PrivateWrapper() {
   const { signed } = store.getState().auth;
