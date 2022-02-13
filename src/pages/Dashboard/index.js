1c97c300eea769fc51a7a94a31742c6a450672cc
Matheus Melo <dev.matheus.melo@gmail.com>
2022-02-13 10:58:45 -0300

    Styling dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft color="#fff" size={36} />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight color="#fff" size={36} />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Matheus Melo</span>
+        </Time>
+        <Time available>
+          <strong>08:00</strong>
+          <span>Matheus Melo</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Matheus Melo</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Matheus Melo</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
